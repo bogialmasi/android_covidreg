88955cdc528c74707747e882efd750268db944a9
Szasza <szalanics.szabolcs@gmail.com>
2021-04-16 16:29:34 +0200

    main szerkesztve

--- app/src/main/java/com/example/covidreg/MainActivity.java
+++ app/src/main/java/com/example/covidreg/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.covidreg;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText email = findViewById(R.id.email);
+        EditText taj = findViewById(R.id.taj);
+        Button btn = findViewById(R.id.btn);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
     }
 }
